31326e0647c16013a1433516d24266e757c10093
Danilo <danilozacyac@gmail.com>
2013-06-26 23:21:30 -0500

    ok

---
+++ GeneraDiccionario/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
+namespace GeneraDiccionario
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSeparar_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> listaPalabras = Utils.StandarizaCadena(TxtTexto.Text).Split(' ').ToList();
+            listaPalabras.Distinct();
+
+            string temp = Path.GetTempFileName() + ".txt";
+
+            TextWriter tw = new StreamWriter(temp, true);
+
+            foreach (string line in listaPalabras)
+            {
+                if(String.IsNullOrWhiteSpace(line) || String.IsNullOrEmpty(line) || line.Length < 4){}
+                else
+                tw.WriteLine(line);
+            }
+
+            
+            tw.Close();
+
+            Process.Start(temp);
+        }
+    }
+}
---
+++ GeneraDiccionario/Utils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneraDiccionario
+{
+    public class Utils
+    {
+
+        public static String StandarizaCadena(String textoOriginal)
+        {
+            textoOriginal = textoOriginal.Replace("á", "a");
+            textoOriginal = textoOriginal.Replace("é", "e");
+            textoOriginal = textoOriginal.Replace("í", "i");
+            textoOriginal = textoOriginal.Replace("ó", "o");
+            textoOriginal = textoOriginal.Replace("ú", "u");
+            textoOriginal = textoOriginal.Replace("Á", "A");
+            textoOriginal = textoOriginal.Replace("É", "E");
+            textoOriginal = textoOriginal.Replace("Í", "I");
+            textoOriginal = textoOriginal.Replace("Ó", "O");
+            textoOriginal = textoOriginal.Replace("Ú", "U");
+            textoOriginal = textoOriginal.Replace(" de ", " ");
+            textoOriginal = textoOriginal.Replace(" el ", " ");
+            textoOriginal = textoOriginal.Replace(" lo ", " ");
+            textoOriginal = textoOriginal.Replace(" la ", " ");
+            textoOriginal = textoOriginal.Replace(" un ", " ");
+            textoOriginal = textoOriginal.Replace(" una ", " ");
+            textoOriginal = textoOriginal.Replace(" es ", " ");
+            textoOriginal = textoOriginal.Replace("[", " ");
+            textoOriginal = textoOriginal.Replace("]", " ");
+            textoOriginal = textoOriginal.Replace("{", " ");
+            textoOriginal = textoOriginal.Replace("}", " ");
+            textoOriginal = textoOriginal.Replace("^", " ");
+            textoOriginal = textoOriginal.Replace("\"", " ");
+            textoOriginal = textoOriginal.Replace("(", " ");
+            textoOriginal = textoOriginal.Replace(")", " ");
+
+
+
+            return textoOriginal;
+        }
+
+    }
+}
